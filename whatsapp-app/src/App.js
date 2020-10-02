1a879adfb560752dc5ce32d53dd141f5eeb1f653
Rafael S. Zanella <rafaels.zanella@gmail.com>
2020-10-01 23:49:11 -0300

    feat(frontend): add pusher.

--- whatsapp-app/src/App.js
+++ whatsapp-app/src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import Pusher from 'pusher-js';
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
 
+
 function App() {
+
+  useEffect(() => {
+    const pusher = new Pusher('917798f1313ea1977ed8', {
+      cluster: 'us2'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, [])
+
   return (
     <div className="app">
       <div className="app__body">
