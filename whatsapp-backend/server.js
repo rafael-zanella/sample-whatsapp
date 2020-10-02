99dab152f402a2209458b822485213ac116a0ad4
Rafael S. Zanella <rafaels.zanella@gmail.com>
2020-10-01 23:08:59 -0300

    feat(backend): add pusher integration to messages collection.

--- whatsapp-backend/server.js
+++ whatsapp-backend/server.js
@@ -2,13 +2,31 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import Pusher from 'pusher'
+import Messages from './dbMessages.js'
+
 dotenv.config()
 
 // app config
 const app = express()
 const port = process.env.PORT || 9000
 
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_APP_KEY,
+  secret: process.env.PUSHER_APP_SECRET,
+  cluster: process.env.PUSHER_APP_CLUSTER,
+  useTSL: process.env.PUSHER_APP_USE_TSL
+});
+
 // middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Acess-Control-Allow-Headers", "*");
+  next();
+})
 
 // DB Config
 const db_user = process.env.DB_USER
@@ -22,10 +40,54 @@ mongoose.connect(db_url, {
   useUnifiedTopology: true
 })
 
-// ????
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('DB connected');
+
+  const msgCollection = db.collection('messagecontents')
+  const changeStream = msgCollection.watch()
+
+  changeStream.on('change', (change) => {
+    if(change.operationType === 'insert') {
+      const messageDetails = change.fullDocument;
+      console.log(`name: ${messageDetails.name} \nmessage: ${messageDetails.message}`)
+      pusher.trigger('messages', 'inserted', {
+        name: messageDetails.name,
+        message: messageDetails.message,
+      });
+      
+    } else {
+      console.log("Error triggering pusher")
+    }
+  })
+})
+
 
 // api routes
 app.get('/', (req, res) => res.status(200).send('Hello World'))
+
+app.get('/messages/sync', (req, res) => {
+  Messages.find((err, data) => {
+    if(err) {
+      res.status(500).send(err)
+    } else {
+      res.status(200).send(data)
+    }
+  })
+})
+
+app.post('/messages/new', (req, res) => {
+  const dbMessage = req.body
+
+  Messages.create(dbMessage, (err, data) => {
+    if(err) {
+      res.status(500).send(err)
+    } else {
+      res.status(201).send(data)
+    }
+  })
+})
 
 // listen
 app.listen(port, () => console.log(`Listening on port: ${port}`))
