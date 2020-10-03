aa7908cf0c855d4bbbb8cc235f4bc4c0a41f0f9a
Rafael S. Zanella <rafaels.zanella@gmail.com>
2020-10-03 18:41:03 -0300

    feat(frontend): add send messages feature.

--- whatsapp-app/src/Chat/index.js
+++ whatsapp-app/src/Chat/index.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import './styles.css';
 import {Avatar, IconButton} from "@material-ui/core";
 import {SearchOutlined, AttachFile, MoreVert, InsertEmoticon, Mic } from "@material-ui/icons";
+import api from '../api';
 
-const Chat = () => {
+
+const Chat = ({ messages }) => {
+
+  const [ input, setInput ] = useState('');
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    await api.post('/messages/new', {
+      message: input,
+      name: "Rafael",
+      timestamp: "Just now",
+      received: true
+    })
+
+    setInput('');
+  }
+
   return(
     <div className="chat">
       <div className="chat__header">
@@ -29,29 +46,18 @@ const Chat = () => {
 
       <div className="chat__body">
 
-        <p className="chat__message">
-          <span className="chat__name">Rafa</span>
-            this is a message
-          <span className="chat__timestamp">
-           {new Date().toUTCString()}
-          </span>
-        </p>
+        {messages.map((msg) => {
+          return (
+            <p key={msg._id} className={`chat__message ${msg.received && "chat__receiver"}`}>
+              <span className="chat__name">{msg.name}</span>
+                {msg.message}
+              <span className="chat__timestamp">
+                {msg.timestamp}
+              </span>
+            </p>
+          )
+        })}
 
-        <p className="chat__message chat__receiver">
-          <span className="chat__name">Rafa</span>
-          this is a message
-          <span className="chat__timestamp">
-           {new Date().toUTCString()}
-         </span>
-        </p>
-
-        <p className="chat__message">
-          <span className="chat__name">Rafa</span>
-          this is a message
-          <span className="chat__timestamp">
-           {new Date().toUTCString()}
-         </span>
-        </p>
       </div>
 
       <div className="chat__footer">
@@ -60,10 +66,11 @@ const Chat = () => {
           <input
             placeholder="Type a message"
             type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
           />
 
-          <button
-            type="submit">
+          <button onClick={sendMessage} type="submit">
             Send a message
           </button>
         </form>
