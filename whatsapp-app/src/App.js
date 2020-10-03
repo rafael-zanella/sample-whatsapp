38c77e252e4987e10c6bae06b98873546cd74652
Rafael S. Zanella <rafaels.zanella@gmail.com>
2020-10-03 16:16:26 -0300

    feat(frontend): get all messages and subscribe to new ones.

--- whatsapp-app/src/App.js
+++ whatsapp-app/src/App.js
@@ -1,11 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Pusher from 'pusher-js';
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
-
+import api from './api';
 
 function App() {
+
+  const [ messages, setMessages ] = useState([]);
+
+  useEffect(() => {
+    api.get('/messages/sync')
+      .then(response => {
+        setMessages(response.data);
+      })
+  }, [])
 
   useEffect(() => {
     const pusher = new Pusher('917798f1313ea1977ed8', {
@@ -13,16 +22,22 @@ function App() {
     });
 
     const channel = pusher.subscribe('messages');
-    channel.bind('inserted', (data) => {
-      alert(JSON.stringify(data));
+    channel.bind('inserted', newMessage => {
+      setMessages([...messages, newMessage]);
     });
-  }, [])
 
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    }
+  }, [messages])
+
+  console.log(messages);
   return (
     <div className="app">
       <div className="app__body">
         <Sidebar />
-        <Chat />
+        <Chat/>
       </div>
     </div>
   );
